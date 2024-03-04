1ee700c1b0e3553ee15ae72c8fca16f6e7ecc71d
kimchiiboiii <matt.hernz@gmail.com>
2024-03-04 16:29:39 -0500

    Added colors, font and border to calculator

--- calcAct.js
+++ calcAct.js
@@ -4,7 +4,12 @@
 
 function getOp(button) {
         
-          if (button == "div") {
+        // function gets called on button click
+        // conditional on the value attribute of the buttons
+        // probably could just use getElementbyId  
+
+    
+        if (button == "div") {
             
             console.log("/");
             return "/";
@@ -27,11 +32,16 @@ function getOp(button) {
         }  else if (button == "equals") {
             
             doMath();
-
+            
+            // should just get rid of this and have the equal sign button itself call doMath on click
+            
+            // this is a problem, its just always going to return equals when u use the calculator
+            // how do i send the return of this function as a parameter and also call doMath when equal sign is clicked
+            
 
         } else {
 
-            console.log("Error: getOp()");
+            console.log("Error with getOp()");
 
         }
 
@@ -41,38 +51,48 @@ function getOp(button) {
 
 function getNumAndClearInput() {
 
+    // this gets called every time they click one of the operations buttons
+    
     let firstNum = document.getElementById("num-field");
     let inputValue = firstNum.value;
     console.log(inputValue); 
     firstNum.value = "";
-    return inputValue;
+    return inputValue; // how do i pass 2 seperate returns of this function as doMath parameters?
 }
 
 
 
 
-function doMath(number1, getOp, number2){
+function doMath(number1, operator, number2){
 
-  if (getOp == "/")   {
+  // number1 and number2 = returns of getNumandClearInput
+  // operator = return of getOp
+  // kind of confused on when i should call this function
+  // and do i just do "doMath(getNumandClearInput(), getOp(), getNumAndClearInput())" ?
+  // ^ probably should just have the equal sign button call this function w those parameters on click
+  
+  
+  
+    if (operator == "/")   {
 
             answer = (number1 / number2);
             console.log(`${number1} divided by ${number2} equals ${answer}`);
             return answer;
             
-  } else if (getOp == "X") {
+  } else if (operator == "X") {
 
             answer = (number1 * number2);
             console.log(`${number1} multiplied by ${number2} equals ${answer}`);
             return answer;
             
-  } else if (getOp == "+") {
+  } else if (operator == "+") {
 
             answer = (number1 + number2);
             console.log(`${number1} plus ${number2} equals ${answer}`);
             return answer;
 
 
-  } else if (getOp == "-") {
+  } else if (operator == "-") {
 
             answer = (number1 - number2) 
             console.log(`${number1} minus ${number2} = ${answer}`);
@@ -82,7 +102,7 @@ function doMath(number1, getOp, number2){
 
     } else {
 
-                console.log("Error: doMath()");
+                console.log("Error with doMath()");
 
             }
 
