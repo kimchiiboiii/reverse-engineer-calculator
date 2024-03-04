53020ea8abdfdb2e10075c37c82e1915b6216eef
kimchiiboiii <matt.hernz@gmail.com>
2024-03-04 12:23:17 -0500

    fixed formatting

--- calcAct.js
+++ calcAct.js
@@ -2,18 +2,36 @@
 
 
 
-function getOp(tag) {
-        if (tag.value == "div") {
-          return "/";
-        } else if (tag.value == "times") {
-            return "X";
-        } else if (tag.value == "subtract") {
-            return "-";
-        } else if (tag.value == "addition") {
-            return "+"
-        } else if (tag.value == "equals") {
+function getOp(button) {
+        
+          if (button == "div") {
             
+            console.log("/");
+            return "/";
+        
+        }  else if (button == "times") {
+            
+            console.log("X");
+            return "X";
+           
+        }  else if (button == "subtract") {
+            
+            console.log("-");
+            return "-";
+        
+        }  else if (button == "addition") {
+            
+            console.log("+");
+            return "+"             
+        
+        }  else if (button == "equals") {
+            
+            doMath();
 
+
+        } else {
+
+            console.log("Error: getOp()");
 
         }
 
@@ -21,49 +39,40 @@ function getOp(tag) {
 
 }
 
-
-
-
-
-
-function clearInput() {
+function getNumAndClearInput() {
 
     let firstNum = document.getElementById("num-field");
     let inputValue = firstNum.value;
     console.log(inputValue); 
     firstNum.value = "";
     return inputValue;
-
-
-
-
-
 }
+
+
 
 
 function doMath(number1, getOp, number2){
 
-          if (getOp == "/")   {
+  if (getOp == "/")   {
 
             answer = (number1 / number2);
-            
             console.log(`${number1} divided by ${number2} equals ${answer}`);
             return answer;
             
-          } else if (getOp == "X") {
+  } else if (getOp == "X") {
 
             answer = (number1 * number2);
             console.log(`${number1} multiplied by ${number2} equals ${answer}`);
             return answer;
             
-          } else if (getOp == "+") {
+  } else if (getOp == "+") {
 
             answer = (number1 + number2);
             console.log(`${number1} plus ${number2} equals ${answer}`);
             return answer;
 
 
-          } else if (getOp == "-") {
+  } else if (getOp == "-") {
 
             answer = (number1 - number2) 
             console.log(`${number1} minus ${number2} = ${answer}`);
@@ -71,57 +80,10 @@ function doMath(number1, getOp, number2){
 
 
 
-            } else {
+    } else {
 
-                console.log("Error");
-
+                console.log("Error: doMath()");
 
             }
 
-
           }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function getOp(operator){
-
-  // if(operator) ==     {
-
-
-
-
-   // }
-
-
-// }
